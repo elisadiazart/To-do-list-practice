88caa22c1087ca1f1df71526072de3786a1d98b7
Elisa <elisadiazartcontact@gmail.com>
2023-02-06 19:01:46 +0100

    06.02.23

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,60 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
 
-sayHello();
+const createTasks = document.getElementById('create');
+const taskContainer = document.getElementById('tasksContainer');
+
+const taskArray = [];
+
+const createObject = task => {
+  const timeStamp = Date.now();
+  const objectTask = {};
+  objectTask.id = timeStamp;
+  objectTask.task = task;
+  objectTask.checked = false;
+  taskArray.push(objectTask);
+  createTask(taskArray);
+};
+
+const printTask = fragment => {
+  taskContainer.innerHTML = '';
+  taskContainer.append(fragment);
+};
+
+const createTask = array => {
+  console.log(array);
+  const fragment = document.createDocumentFragment();
+  array.forEach(task => {
+    const taskContainerDiv = document.createElement('div');
+    const newTask = document.createElement('input');
+    const newTaskLabel = document.createElement('label');
+    const closeElement = document.createElement('span');
+    taskContainerDiv.classList.add('task');
+    closeElement.textContent = 'X';
+    closeElement.classList.add('close');
+    newTask.id = task.id;
+    newTask.type = 'checkbox';
+    newTaskLabel.htmlFor = task.id;
+    newTaskLabel.textContent = task.task;
+    taskContainerDiv.append(newTask, newTaskLabel, closeElement);
+    fragment.append(taskContainerDiv);
+  });
+  printTask(fragment);
+};
+
+const deleteTask = idSelected => {
+  const filteredArray = taskArray.filter(object => object.id !== idSelected);
+  console.log(filteredArray);
+};
+
+createTasks.addEventListener('submit', e => {
+  e.preventDefault();
+  createObject(e.target.task.value);
+  e.target.task.value = '';
+});
+
+taskContainer.addEventListener('click', e => {
+  if (e.target.classList.contains('close')) {
+    deleteTask(Number(e.target.previousSibling.htmlFor));
+  }
+});
