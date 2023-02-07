58b911a760643bbb5c8ab22698b8e9f5f8ffae44
Elisa <elisadiazartcontact@gmail.com>
2023-02-07 17:56:52 +0100

    07.02.23

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,13 @@ import '../scss/styles.scss';
 
 const createTasks = document.getElementById('create');
 const taskContainer = document.getElementById('tasksContainer');
+const itemsLeft = document.getElementById('allItems');
+const filters = document.getElementById('filters');
+const clearCompletedElement = document.getElementById('clearCompleted');
 
-const taskArray = [];
+let taskArray = [];
+let uncheckedArray = [];
+let checkedArray = [];
 
 const createObject = task => {
   const timeStamp = Date.now();
@@ -22,7 +27,6 @@ const printTask = fragment => {
 };
 
 const createTask = array => {
-  console.log(array);
   const fragment = document.createDocumentFragment();
   array.forEach(task => {
     const taskContainerDiv = document.createElement('div');
@@ -34,7 +38,8 @@ const createTask = array => {
     closeElement.classList.add('close');
     newTask.id = task.id;
     newTask.type = 'checkbox';
-    newTaskLabel.classList.add('label')
+    newTask.checked = task.checked;
+    newTaskLabel.classList.add('label');
     newTaskLabel.htmlFor = task.id;
     newTaskLabel.textContent = task.task;
     taskContainerDiv.append(newTask, newTaskLabel, closeElement);
@@ -43,19 +48,89 @@ const createTask = array => {
   printTask(fragment);
 };
 
-const deleteTask = (idSelected, selectedTask) => {
+const deleteTask = idSelected => {
   const filteredArray = taskArray.filter(object => object.id !== idSelected);
-  selectedTask.remove()
+  taskArray = filteredArray;
+  createTask(taskArray);
+};
+
+const setCheckOnTask = taskId => {
+  taskArray.forEach(task => {
+    if (task.id === Number(taskId)) task.checked = !task.checked;
+  });
+  createTask(taskArray);
+};
+
+const numberOfItems = array => {
+  itemsLeft.textContent = `${array.length} items left`;
+};
+
+const onlyCheckedTasks = () => {
+  const filteredArray = taskArray.filter(object => object.checked === false);
+  uncheckedArray = filteredArray;
+  createTask(uncheckedArray);
+};
+
+const onlyCheckedTasksWithoutPrint = () => {
+  const filteredArray = taskArray.filter(object => object.checked === false);
+  uncheckedArray = filteredArray;
+  console.log(uncheckedArray);
+};
+
+const onlyUncheckedTasks = () => {
+  const filteredArray = taskArray.filter(object => object.checked !== false);
+  checkedArray = filteredArray;
+  createTask(checkedArray);
+};
+
+const clearCompleted = () => {
+  const filteredArray = taskArray.filter(object => object.checked === false);
+  taskArray = filteredArray;
+  createTask(taskArray);
 };
 
 createTasks.addEventListener('submit', e => {
   e.preventDefault();
   createObject(e.target.task.value);
   e.target.task.value = '';
+  numberOfItems(taskArray);
 });
 
 taskContainer.addEventListener('click', e => {
   if (e.target.classList.contains('close')) {
-    deleteTask(Number(e.target.previousSibling.htmlFor),e.target.parentElement);
+    deleteTask(
+      Number(e.target.previousSibling.htmlFor),
+      e.target.parentElement,
+      taskArray
+    );
+  } else if ((e.target.type = 'checkbox')) {
+    setCheckOnTask(e.target.id);
+    onlyCheckedTasksWithoutPrint();
+    numberOfItems(uncheckedArray);
   }
+});
+
+filters.addEventListener('click', e => {
+  if (e.target.id === 'filterActive') {
+    filters.children[0].classList.remove('active-filter');
+    filters.children[1].classList.add('active-filter');
+    filters.children[2].classList.remove('active-filter');
+    onlyCheckedTasks();
+    numberOfItems(uncheckedArray);
+  } else if (e.target.id === 'filterAll') {
+    filters.children[0].classList.add('active-filter');
+    filters.children[1].classList.remove('active-filter');
+    filters.children[2].classList.remove('active-filter');
+    createTask(taskArray);
+    numberOfItems(taskArray);
+    onlyCheckedTasksWithoutPrint();
+    numberOfItems(uncheckedArray);
+  } else if (e.target.id === 'filterCompleted') {
+    onlyUncheckedTasks();
+    numberOfItems(checkedArray);
+  }
+});
+
+clearCompletedElement.addEventListener('click', e => {
+  clearCompleted();
 });
